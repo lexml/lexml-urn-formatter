5b57bf65786fae61c85d263c3e78815b201beaa6
laurocesar <laurocesar@gmail.com>
2017-01-30 06:34:41 -0200

    Completa os casos de testes

--- src/test/scala/urn2label/Urn2LabelTest.scala
+++ src/test/scala/urn2label/Urn2LabelTest.scala
@@ -54,17 +54,85 @@ class Urn2LabelTest extends TestCase {
     assertEquals(Fragment2Label.format("art1_par12"), "§ 12º.")
   }
 
+  //incisos
+  def testLabel_art3_par2_inc3() {
+    assertEquals(Fragment2Label.format("art3_par2_inc3"), "III –")
+  }
+  def testLabel_art3_par2_inc3_1() {
+    assertEquals(Fragment2Label.format("art3_par2_inc3-1"), "III-A –")
+  }
 
+  //alíneas
+  def testLabel_art3_par2_inc3_ali4() {
+    assertEquals(Fragment2Label.format("art3_par2_inc3_ali4"), "d)")
+  }
+  def testLabel_art3_par2_inc3_ali5_1() {
+    assertEquals(Fragment2Label.format("art3_par2_inc3_ali5-1"), "e-A)")
+  }
+  def testLabel_art3_par2_inc3_ali5_2() {
+    assertEquals(Fragment2Label.format("art3_par2_inc3_ali5-2"), "e-B)")
+  }
+  def testLabel_art3_par2_inc3_ali5_26() {
+    assertEquals(Fragment2Label.format("art3_par2_inc3_ali5-2"), "e-Z)")
+  }
+  def testLabel_art3_par2_inc3_ali5_27() {
+    assertEquals(Fragment2Label.format("art3_par2_inc3_ali5-2"), "e-AA)")
+  }
   
-      /*
-      , "art1-27"
-      , "art4_par1u"
-      , "art4_par1u_ite7"
-      , "art4_par1u_ite7_ali3"
-      , "art4_par1u_ite7_ali3_inc2"
-      , "art1-1"
-      , "art1-2")
-*/
+  //item
+  def testLabel_art3_par2_inc3_ali4_ite8() {
+    assertEquals(Fragment2Label.format("art3_par2_inc3_ali4_ite8"), "8.")
+  }
+  def testLabel_art3_par2_inc3_ali4_ite8_1() {
+    assertEquals(Fragment2Label.format("art3_par2_inc3_ali4_ite8-1"), "8-A.")
+  }
+  def testLabel_art3_par2_inc3_ali4_ite8_2() {
+    assertEquals(Fragment2Label.format("art3_par2_inc3_ali4_ite8-2"), "8-B.")
+  }
+  def testLabel_art3_par2_inc3_ali4_ite8_26() {
+    assertEquals(Fragment2Label.format("art3_par2_inc3_ali4_ite8-26"), "8-Z.")
+  }
+  def testLabel_art3_par2_inc3_ali4_ite8_28() {
+    assertEquals(Fragment2Label.format("art3_par2_inc3_ali4_ite8-26"), "8-AB.")
+  }
+  
+  //agrupadores
+  def testLabel_tit3_cap3() {
+    assertEquals(Fragment2Label.format("tit3_cap3"), "CAPÍTULO III")
+  }
+
+  def testLabel_tit3_cap3_1() {
+    assertEquals(Fragment2Label.format("tit3_cap3-1"), "CAPÍTULO III-A")
+  }
+  def testLabel_tit3_cap3_27() {
+    assertEquals(Fragment2Label.format("tit3_cap3-27"), "CAPÍTULO III-AA")
+  }
+
+  def testLabel_tit3() {
+    assertEquals(Fragment2Label.format("tit3"), "TÍTULO III")
+  }
+  def testLabel_liv1() {
+    assertEquals(Fragment2Label.format("liv1"), "LIVRO I")
+  }
+  
+  def testLabel_sec1() {
+    assertEquals(Fragment2Label.format("sec1"), "Seção I")
+  }
+  
+  def testLabel_cap2_sec3_sub1() {
+    assertEquals(Fragment2Label.format("cap2_sec3_sub1"), "Subseção I")
+  }
+  
+  // Bloco de alteração
+  def testLabel_art1_par1u_alt1_art32() {
+    assertEquals(Fragment2Label.format("art1_par1u_alt1_art32"), "Art. 32.")
+  }
+  def testLabel_art3_cpt_alt1_art4_par2() {
+    assertEquals(Fragment2Label.format("art3_cpt_alt1_art4_par2"), "§ 2º")
+  }
+  def testLabel_art3_cpt_alt1_tit3() {
+    assertEquals(Fragment2Label.format("art3_cpt_alt1_tit3"), "TÍTULO III")
+  }
 
 
 }
