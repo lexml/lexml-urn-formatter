d967f01426b15c58533bc386de725837df748c76
laurocesar <laurocesar@gmail.com>
2017-01-31 09:03:47 -0200

    Conclui o formatador de URN

--- src/main/scala/br/gov/lexml/urnformatter/Urn2Rotulo.scala
+++ src/main/scala/br/gov/lexml/urnformatter/Urn2Rotulo.scala
@@ -1,8 +1,11 @@
-package urn2label
+package br.gov.lexml.urnformatter
 
 import br.gov.lexml.parser.pl.output.LexmlRenderer
 
-object Fragment2Label {
+/**
+ * Formata uma URN como um rótulo
+ */
+object Urn2Rotulo {
 
   abstract sealed class Numero {
     val n: Int
@@ -37,33 +40,42 @@ object Fragment2Label {
   type FormattedComp = String
 
   val agregadores: Map[String, String] = Map(
-    "prt" -> "Parte",
-    "liv" -> "Livro",
-    "cap" -> "Capítulo",
+    "prt" -> "PARTE",
+    "liv" -> "LIVRO",
+    "cap" -> "CAPÍTULO",
+    "tit" -> "TÍTULO",
     "sec" -> "Seção",
     "sub" -> "Subseção")
 
   def formatComp: Comp => Option[FormattedComp] = {
-    case ("art", Unico :: _) => Some("Art. Único.")
-    case ("art", Algum(n) :: cs) =>
-      Some("Art. " + formatOrdinal(n) + formatComplementos(cs))
     case ("cpt", _) => None
+    case ("alt", _) => None
+    case ("omi", _) => None
+    case ("art", Unico :: _) => Some("Art. Único.")
+    case ("art", Algum(n) :: cs) => 
+        Some("Art. " 
+              +  (if (n< 10){formatOrdinal(n)}else{n}) 
+              + formatComplementos(cs) 
+              + (if (n> 9) {"."} else {""}))
     case ("par", Unico :: _) => Some("Parágrafo Único.")
     case ("par", Algum(n) :: cs) =>
-      Some("§ " + formatOrdinal(n) + formatComplementos(cs))
+      Some("§ " 
+            + (if (n< 10){formatOrdinal(n)} else {n}) 
+            + formatComplementos(cs)
+            + (if (n> 9) {"."} else {""}))
     case ("inc", n :: cs) =>
-      Some(formatRomano(n.n).toUpperCase + formatComplementos(cs))
+      Some(formatRomano(n.n).toUpperCase + formatComplementos(cs) + " –")
     case ("ali", n :: cs) =>
-      Some(formatAlfa(n.n).toLowerCase + formatComplementos(cs))
+      Some(formatAlfa(n.n).toLowerCase + formatComplementos(cs) + ")")
     case ("ite", n :: cs) =>
-      Some(n.n.toString + formatComplementos(cs))
+      Some(n.n.toString + formatComplementos(cs) + ".")
     case (tip, n :: cs) if agregadores contains tip => {
       val t = agregadores(tip)
       val ntxt = n match {
         case Unico => "único"
         case Algum(n) => formatRomano(n).toUpperCase
       }
-      Some(t + " " + ntxt)
+      Some(t + " " + ntxt + formatComplementos(cs))
     }
     case _ => None
   }
@@ -74,7 +86,7 @@ object Fragment2Label {
 
   def formatAlfa(n: Int): String = LexmlRenderer.renderAlphaSeq(n - 1)
 
-  def formatComplementos(cs: List[Numero]): String = cs.map(c => formatComplemento(c.n)).map("-" + _).mkString("")
+  def formatComplementos(cs: List[Numero]): String = cs.map(c => formatComplemento(c.n+1)).map("-" + _).mkString("")
   def formatComplemento(n: Int): String = formatAlfa(n - 1).toUpperCase
 
 }
---
+++ src/test/scala/br/gov/lexml/urnformatter/Urn2RotuloTest.scala
@@ -0,0 +1,165 @@
+package br.gov.lexml.urnformatter
+
+import junit.framework.Assert.assertEquals
+import junit.framework.TestCase
+
+class Urn2RotuloTest extends TestCase {
+  
+  def testLabel_art1() {
+    assertEquals(Urn2Rotulo.format("art1"), "Art. 1º")
+  }
+  
+  def testLabel_art1u() {
+    assertEquals(Urn2Rotulo.format("art1u"), "Art. Único.")
+  }
+  
+  def testLabel_art2() {
+    assertEquals(Urn2Rotulo.format("art2"), "Art. 2º")
+  }
+  
+  def testLabel_art12() {
+    assertEquals(Urn2Rotulo.format("art12"), "Art. 12.")
+  }
+  
+  def testLabel_art1_1() {
+    assertEquals(Urn2Rotulo.format("art1-1"), "Art. 1º-A")
+  }
+  def testLabel_art1_2() {
+    assertEquals(Urn2Rotulo.format("art1-2"), "Art. 1º-B")
+  }
+  def testLabel_art1_27() {
+    assertEquals(Urn2Rotulo.format("art1-27"), "Art. 1º-AA")
+  }
+  def testLabel_art1_28() {
+    assertEquals(Urn2Rotulo.format("art1-28"), "Art. 1º-AB")
+  }
+ 
+  // parágrafo
+  def testLabel_art1_par1u() {
+    assertEquals(Urn2Rotulo.format("art1_par1u"), "Parágrafo Único.")
+  }
+  def testLabel_art1_par1() {
+    assertEquals(Urn2Rotulo.format("art1_par1"), "§ 1º")
+  }
+  def testLabel_art1_par2() {
+    assertEquals(Urn2Rotulo.format("art1_par2"), "§ 2º")
+  }
+  def testLabel_art1_par9() {
+    assertEquals(Urn2Rotulo.format("art1_par9"), "§ 9º")
+  }
+  def testLabel_art1_par9_4() {
+    assertEquals(Urn2Rotulo.format("art1_par9-4"), "§ 9º-D")
+  }
+  def testLabel_art1_par10() {
+    assertEquals(Urn2Rotulo.format("art1_par10"), "§ 10.")
+  }
+  def testLabel_art1_par12() {
+    assertEquals(Urn2Rotulo.format("art1_par12"), "§ 12.")
+  }
+  def testLabel_art1_par12_3() {
+    assertEquals(Urn2Rotulo.format("art1_par12-3"), "§ 12-C.")
+  }
+
+  //incisos
+  def testLabel_art3_par2_inc3() {
+    assertEquals(Urn2Rotulo.format("art3_par2_inc3"), "III –")
+  }
+  def testLabel_art3_par2_inc3_1() {
+    assertEquals(Urn2Rotulo.format("art3_par2_inc3-1"), "III-A –")
+  }
+
+  //alíneas
+  def testLabel_art3_par2_inc3_ali4() {
+    assertEquals(Urn2Rotulo.format("art3_par2_inc3_ali4"), "d)")
+  }
+  def testLabel_art3_par2_inc3_ali5_1() {
+    assertEquals(Urn2Rotulo.format("art3_par2_inc3_ali5-1"), "e-A)")
+  }
+  def testLabel_art3_par2_inc3_ali5_2() {
+    assertEquals(Urn2Rotulo.format("art3_par2_inc3_ali5-2"), "e-B)")
+  }
+  def testLabel_art3_par2_inc3_ali5_26() {
+    assertEquals(Urn2Rotulo.format("art3_par2_inc3_ali5-26"), "e-Z)")
+  }
+  def testLabel_art3_par2_inc3_ali5_27() {
+    assertEquals(Urn2Rotulo.format("art3_par2_inc3_ali5-27"), "e-AA)")
+  }
+  
+  //item
+  def testLabel_art3_par2_inc3_ali4_ite8() {
+    assertEquals(Urn2Rotulo.format("art3_par2_inc3_ali4_ite8"), "8.")
+  }
+  def testLabel_art3_par2_inc3_ali4_ite8_1() {
+    assertEquals(Urn2Rotulo.format("art3_par2_inc3_ali4_ite8-1"), "8-A.")
+  }
+  def testLabel_art3_par2_inc3_ali4_ite8_2() {
+    assertEquals(Urn2Rotulo.format("art3_par2_inc3_ali4_ite8-2"), "8-B.")
+  }
+  def testLabel_art3_par2_inc3_ali4_ite8_26() {
+    assertEquals(Urn2Rotulo.format("art3_par2_inc3_ali4_ite8-26"), "8-Z.")
+  }
+  def testLabel_art3_par2_inc3_ali4_ite8_27() {
+    assertEquals(Urn2Rotulo.format("art3_par2_inc3_ali4_ite8-27"), "8-AA.")
+  }
+  def testLabel_art3_par2_inc3_ali4_ite8_28() {
+    assertEquals(Urn2Rotulo.format("art3_par2_inc3_ali4_ite8-28"), "8-AB.")
+  }
+  def testLabel_art3_par2_inc3_ali4_ite8_29() {
+    assertEquals(Urn2Rotulo.format("art3_par2_inc3_ali4_ite8-29"), "8-AC.")
+  }
+  def testLabel_art3_par2_inc3_ali4_ite8_30() {
+    assertEquals(Urn2Rotulo.format("art3_par2_inc3_ali4_ite8-30"), "8-AD.")
+  }
+  
+  //agrupadores
+  def testLabel_tit3_cap3() {
+    assertEquals(Urn2Rotulo.format("tit3_cap3"), "CAPÍTULO III")
+  }
+
+  def testLabel_tit3_cap3_1() {
+    assertEquals(Urn2Rotulo.format("tit3_cap3-1"), "CAPÍTULO III-A")
+  }
+  def testLabel_tit3_cap3_27() {
+    assertEquals(Urn2Rotulo.format("tit3_cap3-27"), "CAPÍTULO III-AA")
+  }
+
+  def testLabel_tit3() {
+    assertEquals(Urn2Rotulo.format("tit3"), "TÍTULO III")
+  }
+  def testLabel_liv1() {
+    assertEquals(Urn2Rotulo.format("liv1"), "LIVRO I")
+  }
+  
+  def testLabel_sec1() {
+    assertEquals(Urn2Rotulo.format("sec1"), "Seção I")
+  }
+  
+  def testLabel_cap2_sec3_sub1() {
+    assertEquals(Urn2Rotulo.format("cap2_sec3_sub1"), "Subseção I")
+  }
+  
+  // Bloco de alteração
+  def testLabel_art1_par1u_alt1_art32() {
+    assertEquals(Urn2Rotulo.format("art1_par1u_alt1_art32"), "Art. 32.")
+  }
+  def testLabel_art3_cpt_alt1_art4_par2() {
+    assertEquals(Urn2Rotulo.format("art3_cpt_alt1_art4_par2"), "§ 2º")
+  }
+  def testLabel_art3_cpt_alt1_tit3() {
+    assertEquals(Urn2Rotulo.format("art3_cpt_alt1_tit3"), "TÍTULO III")
+  }
+  
+  // Vazios
+  def testLabel_art1_par1u_alt1() {
+    assertEquals(Urn2Rotulo.format("art1_par1u_alt1"), "")
+  }
+  def testLabel_art1_par1u_omi() {
+    assertEquals(Urn2Rotulo.format("art1_par1u_omi1"), "")
+  }
+  def testLabel_art1_par1u_cpt() {
+    assertEquals(Urn2Rotulo.format("art1_par1u_cpt"), "")
+  }
+
+
+
+}
