17e394551aff6c1e40e583bffe0facf800921e32
laurocesar <laurocesar@gmail.com>
2020-04-16 11:59:20 -0300

    Acrescenta novos casos

--- src/test/scala/br/gov/lexml/urnformatter/Uren2NomeTestCompacto.scala
+++ src/test/scala/br/gov/lexml/urnformatter/Uren2NomeTestCompacto.scala
@@ -311,11 +311,14 @@ class Uren2NomeCompactoTest extends TestCase {
     
     def testLabel_art135711() { assertEquals("arts. 1º, 3º, 5º, 7º e 11", Urn2NomeCompacto.format(List("tit1_sec1_art1", "tit1_sec1_art3", "tit1_sec1_art5", "art7", "tit50_sec1_art11"))) }
     
-    def testLabel_art135711anx1() { assertEquals("arts. 1º, 3º, 5º, 7º e 11 e art. 11 do Anexo I", Urn2NomeCompacto.format(List("tit1_sec1_art1", "tit1_sec1_art3", "tit1_sec1_art5", "art7", "tit50_sec1_art11", "anx1_art1"))) }    
+    def testLabel_art135711anx1() { assertEquals("arts. 1º, 3º, 5º, 7º e 11 e art. 11 do Anexo I", Urn2NomeCompacto.format(List("tit1_sec1_art1", "tit1_sec1_art3", "tit1_sec1_art5", "art7", "tit50_sec1_art11", "anx1_art11"))) }    
 
     def testLabel_anx10() { assertEquals("Anexo X", Urn2NomeCompacto.format(List("anx10"))) }
     
     def testLabel_anx1011() { assertEquals("Anexos X e XI", Urn2NomeCompacto.format(List("anx10", "anx11"))) }
+    
+    def testLabel_art135711anx1tit() { assertEquals("arts. 1º, 3º, 5º, 7º e 11, art. 11 a 15, art. 17, Título 10 e art. 25 do Anexo I", Urn2NomeCompacto.format(List("tit1_sec1_art1", "tit1_sec1_art3", "tit1_sec1_art5", "art7", "tit50_sec1_art11", "anx1_art11", "anx1_art15", "anx1_art17",  "anx1_tit10", "anx1_art25"))) }    
+
 
     
 }
