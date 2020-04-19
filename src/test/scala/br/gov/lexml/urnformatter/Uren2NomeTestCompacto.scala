c0443f513501f4fbbf101277c08c211b8930be2c
laurocesar <laurocesar@gmail.com>
2020-04-19 16:25:02 -0300

    Adiciona mais casos
        def testLabel_art1_43() { assertEquals("arts. 1º a 21, 28 a 38,
    38-A, 39 a 43", Urn2NomeCompacto.format(List("art1", "art2", "art3",
    "art4", "art5", "art6", "art7", "art8", "art9", "art10", "art11",
    "art12", "art13", "art14", "art15", "art16", "art17", "art18", "art19",
    "art20", "art21", "art28", "art29", "art30", "art31", "art32", "art33",
    "art34", "art35", "art36", "art37", "art38", "art38-A", "art39",
    "art40", "art41", "art42", "art43"))) }
    
        def testLabel_art1_3A_6() { assertEquals("arts. 1º a 3, 3-A, 4 a 6",
    Urn2NomeCompacto.format(List("art1", "art2", "art3", "art3-A", "art4",
    "art5", "art6"))) }

--- src/test/scala/br/gov/lexml/urnformatter/Uren2NomeTestCompacto.scala
+++ src/test/scala/br/gov/lexml/urnformatter/Uren2NomeTestCompacto.scala
@@ -327,5 +327,8 @@ class Uren2NomeCompactoTest extends TestCase {
 
     def testLabel_tit1013() { assertEquals("Títulos X e XIII", Urn2NomeCompacto.format(List("tit10", "tit13"))) }
 
+    def testLabel_art1_43() { assertEquals("arts. 1º a 21, 28 a 38, 38-A, 39 a 43", Urn2NomeCompacto.format(List("art1", "art2", "art3", "art4", "art5", "art6", "art7", "art8", "art9", "art10", "art11", "art12", "art13", "art14", "art15", "art16", "art17", "art18", "art19", "art20", "art21", "art28", "art29", "art30", "art31", "art32", "art33", "art34", "art35", "art36", "art37", "art38", "art38-A", "art39", "art40", "art41", "art42", "art43"))) }
+
+    def testLabel_art1_3A_6() { assertEquals("arts. 1º a 3, 3-A, 4 a 6", Urn2NomeCompacto.format(List("art1", "art2", "art3", "art3-A", "art4", "art5", "art6"))) }
     
 }
