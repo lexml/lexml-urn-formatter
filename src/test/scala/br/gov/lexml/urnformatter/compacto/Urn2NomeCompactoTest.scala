49aef0f77fdad10c04e9ce4ae63b6314cf446ceb
Luciano Molinari <lucmolinari@gmail.com>
2022-07-01 08:45:36 +0200

    Add new test

--- src/test/scala/br/gov/lexml/urnformatter/compacto/Urn2NomeCompactoTest.scala
+++ src/test/scala/br/gov/lexml/urnformatter/compacto/Urn2NomeCompactoTest.scala
@@ -1364,6 +1364,11 @@ class Urn2NomeCompactoTest extends TestCase {
     assertEquals("inciso I", Urn2NomeCompacto.format(List("anx;XPTO_art9_inc1"), "anx;XPTO_art9_cpt"));
   }
 
+  //TODO: inciso I OU inciso I do caput?
+  def test_anxXPTO_art9_cpt_inc1_anxXPTO_art9_cpt() {
+    assertEquals("inciso I", Urn2NomeCompacto.format(List("anx;XPTO_art9_cpt_inc1"), "anx;XPTO_art9_cpt"))
+  }
+
   def test_anx8_art9_inc1_anx8_art9_inc2() {
     assertEquals("inciso I", Urn2NomeCompacto.format(List("anx8_art9_inc1"), "anx8_art9_inc2"));
   }
